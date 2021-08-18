23b209ed2e62493399ce15c5a7113943c0071171
Marius Duboule <dev@mariusduboule.com>
2021-08-18 08:43:20 -0400

    Add more npm, git and utility aliases. Setup nvim 0.5

--- shellconfig/aliases/general.zsh
+++ shellconfig/aliases/general.zsh
@@ -1,7 +1,13 @@
+alias {vi,vim,nvim}='$HOME/nvim-osx64/bin/nvim'
+
 alias sz='source ~/.config/.zshrc'
+alias ez='nvim ~/.config/.zshrc'
 
 alias md=mkdir
 alias rd=rmdir
+alias ls='ls -G -p'
+alias ll='ls -l'
+alias la='ls -a'
 alias ..='cd ..'
 alias ...='cd ../..'
 alias ....='cd ../../..'
