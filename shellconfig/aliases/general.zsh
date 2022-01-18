0c43a534cd133e83b685a54e7c7805f12eae2d8e
Marius Duboule <dev@mariusduboule.com>
2022-01-17 23:41:40 -0500

    Brew: setup 'brow', an Old intel based version of brew

--- shellconfig/aliases/general.zsh
+++ shellconfig/aliases/general.zsh
@@ -1,4 +1,13 @@
-alias {vi,vim,nvim}='/usr/local/bin/nvim'
+alias {vi,vim,nvim}='/opt/homebrew/bin/nvim'
+
+# Alias for Intel based Homebrew install
+alias brow='arch --x86_64 /usr/local/Homebrew/bin/brew'
+
+# Ref: https://stackoverflow.com/questions/64951024/how-can-i-run-two-isolated-installations-of-homebrew
+# I also created another handy alias in .zshrc
+# so I can prepend any Homebrew-installed command
+# with ib to force using the Intel version of that command
+alias ib='PATH=/usr/local/bin'
 
 alias sz='source ~/.config/.zshrc'
 alias ez='nvim ~/.config/.zshrc'
