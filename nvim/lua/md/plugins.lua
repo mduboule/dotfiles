f953e2b48fd7f8a446253be26db1c4ba3c4a8403
Marius Duboule <dev@mariusduboule.com>
2022-05-28 23:25:03 -0400

    feat(nvim): add gitsigns

--- nvim/lua/md/plugins.lua
+++ nvim/lua/md/plugins.lua
@@ -61,6 +61,7 @@ return packer.startup(function(use)
   -- Git
   use 'ThePrimeagen/git-worktree.nvim'
   -- use 'f-person/git-blame.nvim'
+  use 'lewis6991/gitsigns.nvim'
 
   -- Telescope
   use "nvim-telescope/telescope.nvim"
