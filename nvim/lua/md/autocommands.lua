ebb9cf7382d21ddbb2fb43161002d410812a8858
Marius Duboule <dev@mariusduboule.com>
2022-06-03 23:39:35 -0400

    fix(nvim): minor improvements

--- nvim/lua/md/autocommands.lua
+++ nvim/lua/md/autocommands.lua
@@ -36,7 +36,7 @@ vim.api.nvim_create_autocmd({ "FileType" }, {
 
 vim.api.nvim_create_autocmd({ "TextYankPost" }, {
   callback = function()
-    vim.highlight.on_yank { higroup = "Visual", timeout = 200 }
+    vim.highlight.on_yank { higroup = "Visual", timeout = 120 }
   end,
 })
 
