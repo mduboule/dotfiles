d68d32e8c32eb3bf3a1fef4f0b8f6e3ea5c27ac5
Marius Duboule <dev@mariusduboule.com>
2022-06-20 18:56:45 -0400

    refactor(s.sh): apply formatting, add .editorconfig file

--- bin/s.sh
+++ bin/s.sh
@@ -8,7 +8,6 @@
 # TO DO
 # - Improve change of working directory using z earlier in the script, so
 #   that new windows are already in the right directory.
-# - We can probably refactor some of this.
 #
 # Useful resources
 # https://newbedev.com/create-new-tmux-session-from-inside-a-tmux-session
@@ -21,57 +20,57 @@ z_value=$(/bin/zsh -i -c "z -e $1")
 
 # test for empty argument
 if [ -z "$1" ]; then
-	echo "(s.sh) Error: you need to pass a non empty value for the script to work."
-	exit 1
+  echo "(s.sh) Error: you need to pass a non empty value for the script to work."
+  exit 1
 # test for empty value when searching using z-jump
 elif [ -z "$z_value" ]; then
-	echo "(s.sh) Error: couldn't find a match while searching the z catalog."
-	exit 2
+  echo "(s.sh) Error: couldn't find a match while searching the z catalog."
+  exit 2
 fi
 
 # Check if tmux session already exists
 if [ "$TERM_PROGRAM" != tmux ]; then
   is_new_session=true
 else
-  session_exists=$(tmux list-sessions | grep $1)
+  session_exists=$(tmux list-sessions | grep "$1")
 fi
 
 # if tmux is active and we already have a session w same name
-if [ "$is_new_session" != true ] && [ "$session_exists" != "" ] ; then
+if [ "$is_new_session" != true ] && [ "$session_exists" != "" ]; then
   echo 'Switching to pre-existing session'
-  tmux switch-client -t $1:1
+  tmux switch-client -t "$1":1
   echo 'Exit 0'
   exit 0
 fi
 
 # if this is a new tmux session, then create one from scratch
-if [ "$is_new_session" = true ] ; then
+if [ "$is_new_session" = true ]; then
   echo "new session"
-  tmux new -s $1 -n 'git' \; \
+  tmux new -s "$1" -n 'git' \; \
     split-window -h \; \
     new-window -n 'dev' \; \
     new-window -n 'npm' \; \
     split-window -h \; \
-    send-keys -t $1:1.0 "z $1 && clear" C-m \; \
-    send-keys -t $1:1.1 "z $1 && clear" C-m \; \
-    send-keys -t $1:2.0 "z $1 && clear && vi" C-m \; \
-    send-keys -t $1:3.0 "z $1 && clear" C-m \; \
-    send-keys -t $1:3.1 "z $1 && clear" C-m \; \
-    switch-client -t $1:1 \; \
+    send-keys -t "$1":1.0 "z $1 && clear" C-m \; \
+    send-keys -t "$1":1.1 "z $1 && clear" C-m \; \
+    send-keys -t "$1":2.0 "z $1 && clear && vi" C-m \; \
+    send-keys -t "$1":3.0 "z $1 && clear" C-m \; \
+    send-keys -t "$1":3.1 "z $1 && clear" C-m \; \
+    switch-client -t "$1":1 \;
 
 # but if tmux is already active just add another session
 else
   echo "add new session"
-  tmux new-session -s $1 -d
-  tmux rename-window -t $1:1 'git'
-  tmux split-window -h -t $1:1
-  tmux new-window -n 'dev' -t $1:
-  tmux new-window -n 'npm' -t $1:
-  tmux split-window -h -t $1:3
-  tmux send-keys -t $1:1.0 "z $1 && clear" C-m
-  tmux send-keys -t $1:1.1 "z $1 && clear" C-m
-  tmux send-keys -t $1:2.0 "z $1 && clear && vi" C-m
-  tmux send-keys -t $1:3.0 "z $1 && clear" C-m
-  tmux send-keys -t $1:3.1 "z $1 && clear" C-m
-  tmux switch-client -t $1:1
+  tmux new-session -s "$1" -d
+  tmux rename-window -t "$1":1 'git'
+  tmux split-window -h -t "$1":1
+  tmux new-window -n 'dev' -t "$1":
+  tmux new-window -n 'npm' -t "$1":
+  tmux split-window -h -t "$1":3
+  tmux send-keys -t "$1":1.0 "z $1 && clear" C-m
+  tmux send-keys -t "$1":1.1 "z $1 && clear" C-m
+  tmux send-keys -t "$1":2.0 "z $1 && clear && vi" C-m
+  tmux send-keys -t "$1":3.0 "z $1 && clear" C-m
+  tmux send-keys -t "$1":3.1 "z $1 && clear" C-m
+  tmux switch-client -t "$1":1
 fi
